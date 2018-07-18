00dbac588dba85458d7491d5a52e3519af171968
Μιχάλης Τσουγκράνης <infoerawebdesign@gmail.com>
2018-07-18 09:17:54 +0300

    - Added bootstrap code and validation to all basic forms

--- actionmanagementapp/static/js/form_validation.js
+++ actionmanagementapp/static/js/form_validation.js
@@ -4,6 +4,31 @@ Javascript file that contains validation rules for the various forms
 
 
 /**
+function that gets the number of digits, given the input element
+*/
+function getNoOfDigits(el){
+	if (el.value.match(/\d/g))
+		var digitsNumber = el.value.match(/\d/g).length;
+	else  // if regexp result is null, set to zero
+		var digitsNumber = 0;
+	return digitsNumber;
+}
+
+
+/**
+function that gets the number of spaces, given the input element
+*/
+function getNoOfSpaces(el){
+	if (el.value.match(/ /g))
+		var spacesNumber = el.value.match(/ /g).length;
+	else // if regexp result is null, set to zero
+		var spacesNumber = 0;
+		
+	return spacesNumber;
+}
+
+
+/**
 function that validates phone numbers. Takes as input the form input element that contains the phone number.
 It is called by validateFormFields() function
 */
@@ -16,16 +41,10 @@ function validatePhoneNumber(el){
 	//get the length of the phone number
 	var phoneNumberLength = el.value.length;
 	//get the number of digits
-	if (el.value.match(/\d/g))
-		var digitsNumber = el.value.match(/\d/g).length;
-	else  // if regexp result is null, set to zero
-		var digitsNumber = 0;
-	
+	var digitsNumber = getNoOfDigits(el);
 	//get the number of spaces
-	if (el.value.match(/ /g))
-		var spacesNumber = el.value.match(/ /g).length;
-	else // if regexp result is null, set to zero
-		var spacesNumber = 0;
+	var spacesNumber = getNoOfSpaces(el);
+	
 	
 	// if the user has typed a phone number and it is invalid
     if ( (phoneNumberLength>0) && ((phoneNumberLength < 10) || (phoneNumberLength != digitsNumber+spacesNumber)) )
@@ -38,6 +57,33 @@ function validatePhoneNumber(el){
     {
 		el.setCustomValidity('');
     }
+}
+
+
+/**
+function that validates Greek irs numbers. Takes as input the form input element that contains the irs number.
+It is called by validateFormFields() function
+*/
+function validateIrsNumber(el)
+{
+	/* in order for an irs number to be valid, it must have exactly 9 digits. Only spaces are allowed */
+	
+	//get the length of the irs number
+	var irsNumberLength = el.value.length;
+	//get the number of digits
+	var digitsNumber = getNoOfDigits(el);
+	//get the number of spaces
+	var spacesNumber = getNoOfSpaces(el);
+	
+	// if the irs number is valid, reset the custom validity message
+	if ( (irsNumberLength == 0) || ( (digitsNumber == 9) && (digitsNumber+spacesNumber == irsNumberLength) ) )
+	{
+		el.setCustomValidity('');
+	}
+	else
+	{
+		el.setCustomValidity('Το ΑΦΜ δεν είναι έγκυρο. Απαιτούνται ακριβώς 9 ψηφία. Μόνο οι κενοί χαρακτήρες επιτρέπονται.');
+	}
 }
 
 
@@ -58,10 +104,16 @@ function validateFormFields(okButtonElement){
     
 
     for (var i=0; i<formInputs.length; i++){
-        //for each phone number (that has tel as type), add validation
+        // for each phone number (that has tel as type), add validation
 		if (formInputs[i].type == 'tel')
 		{
 			validatePhoneNumber(formInputs[i]);
+		}
+		
+		// for each irs number (that has class irs-no), add validation
+		if (formInputs[i].classList.contains('irs-no'))
+		{
+			validateIrsNumber(formInputs[i]);
 		}
     }
 
